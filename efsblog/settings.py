98691360311e9da4578c7223775a458e7324545b
minhduong <mduong@unomaha.edu>
2018-01-28 15:07:02 -0600

    Updated

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'efsblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Minhduong$assignment1',
+        'USER': 'Minhduong',
+        'PASSWORD': 'Lovemylife243?',
+        'HOST': 'Minhduong.mysql.pythonanywhere-services.com',
     }
 }
 
